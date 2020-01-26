aacb6fcc2ef1f7ba4d343e1465231f1ad2f0d124
Hitesh Songra <hgsongra@ymail.com>
2020-01-26 23:36:49 +0530

    Update help description

--- lib/helper.rb
+++ lib/helper.rb
@@ -2,13 +2,15 @@ module Helper
   ARG_MISSING_ERROR = "Argument(s) missing"
 
   def help
+    puts "Usage: hql [option] [arguments]"
+    puts "    eg. hql add name=anyname age=23 location=\"JP Nagar\"  --> Will add new record"
+    puts "    eg. hql -l    List all records"
     puts "Commands:"
-    puts "    help                                            For help"
-    puts "    add key=value [key2=value2 key3=value3 ...]     Add new record"
-    puts "    delete key=value                                Delete a record or records by a key-value pair"
-    puts "    find value [fields=field1,field2,...]           Find all the records which contain a particular value"
-    puts "    list                                            List all the records"
-    puts "    exit                                            Exit from terminal or press CTRL+^C"
+    puts "    help                                            For help, short -h"
+    puts "    add key=value [key2=value2 key3=value3 ...]     Add new record, short -a"
+    puts "    delete key=value                                Delete a record or records by a key-value pair, short -d"
+    puts "    find value [fields=field1,field2,...]           Find all the records which contain a particular value, short -f"
+    puts "    list                                            List all the records, short -l"
   end
 
   def args_missing?(args) 

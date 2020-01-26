4b3f0d5254dc39f47570e3fb90d8a1452436d737
Hitesh Songra <hgsongra@ymail.com>
2020-01-26 10:33:48 +0530

    Add list of commands and auto complete script

--- server.rb
+++ server.rb
@@ -1,4 +1,11 @@
 require "readline"
 
+LIST = ['add', 'delete', 'find', 'help', 'exit'].sort
+# To auto complete command
+comp = proc { |s| LIST.grep(/^#{Regexp.escape(s)}/) }
+Readline.completion_append_character = " "
+Readline.completion_proc = comp
+
 while input = Readline.readline("$ ", true)
+  puts input
 end
